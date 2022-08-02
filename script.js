236707ecbf23c7d743ee45304d21623487307c74
roche <rochedjohnson@gmail.com>
2022-08-02 21:20:45 +0200

    firstdump

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Hellooo World!")
+
+const strawberryMilkshake="tastee"
+
+console.log("milkshakes are",strawberryMilkshake)
+
+let myAge=23
+myAge = myAge + 6
+
+let camsAge = 50
+camsAge = camsAge - 1
+
+console.log("I hate that I am",myAge)
+
+console.log("camsAge",camsAge)
+
+const shoppingList = ["eggs","milk","rice","flour","chocolate"]
+
+const steven = {
+    hairColor: "black",
+    age: 14,
+    gemFriends: ["garnet","pearl","amethyst"],
+    mom: {
+        name: "rose",
+        hairColor: "pink",
+    }
+}
+console.log(steven)
